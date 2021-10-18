2ec5d6fa7ef7b23286195968d536093e22217475
miloslavsky <miloslavsky9000@gmail.com>
2021-10-18 20:35:37 +0300

    jopa added

--- wall_builder/__init__.py
+++ wall_builder/__init__.py
@@ -19,59 +19,20 @@ from bpy.types import NodesModifier
 sys.path.append('C:\\code\\big_blender_plugin\\wall_builder')
 import data_types
 
+import wb_properties
 import panel
 import wb_operators
 importlib.reload(panel)
 importlib.reload(wb_operators)
+importlib.reload(wb_properties)
 
 
 
 def register():
-
-    bpy.types.Object.customer = bpy.props.EnumProperty(
-        name='customer preset',
-        items=data_types.customers
-    )
-
-    bpy.types.Object.outer_wall_thickness = bpy.props.FloatProperty(
-        name='outer wall thickness',
-        default=0
-    )
-
-    bpy.types.Object.inner_wall_thickness = bpy.props.FloatProperty(
-        name='inner wall thickness',
-        default=0
-    )
-
-    bpy.types.Object.wall_height = bpy.props.FloatProperty(
-        name='wall height',
-        default=0
-    )
-
-    bpy.types.Object.opening_elevation = bpy.props.FloatProperty(
-        name='openings aver elevation',
-        default=0
-        )
-
-    bpy.types.Object.opening_type = bpy.props.EnumProperty(
-        name='opening type',
-        items=data_types.openings_types
-    )
-
-    bpy.types.Object.opening_top_offset = bpy.props.FloatProperty(
-        name='opening top offset',
-        default=0
-    )
-
-    bpy.types.Object.level = bpy.props.IntProperty(
-        name='object level',
-        default=0
-    )
-
     
     classes = [
         panel.MainMenu,
-        wb_operators.WallBuilder
+        wb_operators.WallBuilder,
     ]
     for cls in classes:
         bpy.utils.register_class(cls)
---
+++ wall_builder/wb_properties.py
@@ -0,0 +1,58 @@
+import bpy
+import data_types
+
+def register():
+
+    bpy.types.Object.customer = bpy.props.EnumProperty(
+            name='customer preset',
+            items=data_types.customers
+        )
+
+    bpy.types.Object.outer_wall_thickness = bpy.props.FloatProperty(
+            name='outer wall thickness',
+            default=0
+        )
+
+    bpy.types.Object.inner_wall_thickness = bpy.props.FloatProperty(
+            name='inner wall thickness',
+            default=0
+        )
+
+    bpy.types.Object.wall_height = bpy.props.FloatProperty(
+            name='wall height',
+            default=0
+        )
+
+    bpy.types.Object.opening_elevation = bpy.props.FloatProperty(
+            name='openings aver elevation',
+            default=0
+            )
+
+    bpy.types.Object.opening_type = bpy.props.EnumProperty(
+            name='opening type',
+            items=data_types.openings_types
+        )
+
+    bpy.types.Object.opening_top_offset = bpy.props.FloatProperty(
+            name='opening top offset',
+            default=0
+        )
+
+    bpy.types.Object.level = bpy.props.IntProperty(
+            name='object level',
+            default=0
+        )
+
+def unregister():
+
+    del bpy.types.Object.level
+    del bpy.types.Object.opening_top_offset
+    del bpy.types.Object.opening_type
+    del bpy.types.Object.opening_elevation
+    del bpy.types.Object.wall_height
+    del bpy.types.Object.inner_wall_thickness
+    del bpy.types.Object.outer_wall_thickness
+    del bpy.types.Object.customer
+
+if __name__ == '__main__':
+    register()
